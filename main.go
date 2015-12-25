1ed96b21a0b60a2a6c33932786d2baf40766d15d
T.J. Mazeika <t@mazeika.me>
2015-12-25 17:27:40 -0500

    Fix protocol

--- main.go
+++ main.go
@@ -4,111 +4,101 @@ import (
     "net"
     "fmt"
     "os"
-    /*"time"*/
     "math/rand"
+    "time"
+    "sync"
 )
 
 // protocol information
 const (
     ProtoPort uint16 =  50977
     ProtoPortStr     = "50977"
-    ProtoPeerStrLen  = 16
+    ProtoPeerUIDLen  = 16
 )
 
 type ProtoMsg byte
 
 // protocol messages
 const (
-    /*ProtoMsgPing ProtoMsg = 0x00
-    ProtoMsgPong ProtoMsg = 0x01*/
+    ProtoMsgClientTypeDL ProtoMsg = 0x00
+    ProtoMsgClientTypeUL ProtoMsg = 0x01
 )
 
+var awaitingConns = map[string]net.Conn{}
+var awaitingConnsMutex = &sync.Mutex{}
+
 func main() {
-    laddr, err := net.ResolveTCPAddr("tcp", net.JoinHostPort("", ProtoPortStr))
+    listener, err := net.Listen("tcp", net.JoinHostPort("", ProtoPortStr))
 
     if err != nil {
         fmt.Fprintln(os.Stderr, err)
         os.Exit(1)
     }
-
-    listener, err := net.ListenTCP("tcp", laddr)
-
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
-
-    ch := make(chan *net.TCPConn)
-
-    awaitingConns := map[string]net.TCPConn{}
-
-    go func() {
-        for {
-            conn := <- ch
-
-            if err != nil {
-                fmt.Fprintln(os.Stderr, err)
-                continue
-            }
-
-            var uid string
-            /*var connClosed bool*/
-
-            for {
-                uid = randSeq(ProtoPeerStrLen)
-
-                if awaitingConns[uid] == nil {
-                    awaitingConns[uid] = conn
-                    break
-                }
-            }
-
-
-
-            // pinging
-            /*go func() {
-                pongBuffer := make([]byte, 1)
-
-                go func() {
-                    for ! connClosed {
-                        time.Sleep(time.Second * 30)
-
-                        if pongBuffer[0] != byte(ProtoMsgPong) {
-                            conn.Close()
-                            connClosed = true
-                        }
-                    }
-                }()
-
-                for ! connClosed {
-                    conn.Write([]byte{byte(ProtoMsgPing)})
-                    conn.Read(pongBuffer)
-                    time.Sleep(time.Second * 30)
-                }
-            }()*/
-        }
-    }()
 
     for {
-        conn, err := listener.AcceptTCP()
+        conn, err := listener.Accept()
 
-        if err != nil {
-            fmt.Fprintln(os.Stderr, err)
-            continue
-        }
+        go func() {
+            if err != nil {
+                fmt.Fprintln(os.Stderr, err)
+                return
+            }
 
-        fmt.Println("Connection from", conn.RemoteAddr().String())
-        ch <- conn
+            clientTypeBuffer := make([]byte, 1)
+            conn.Read(clientTypeBuffer)
+
+            switch ProtoMsg(clientTypeBuffer[0]) {
+            case ProtoMsgClientTypeDL:
+                go handleDownloader(conn)
+            case ProtoMsgClientTypeUL:
+                go handleUploader(conn)
+            default:
+                fmt.Fprintln(os.Stderr, "Protocol error")
+            }
+        }()
     }
 }
 
+func handleDownloader(conn net.Conn) {
+    var uid string
+    awaitingConnsMutex.Lock()
+
+    for uid == "" || awaitingConns[uid] != nil {
+        uid = randSeq(ProtoPeerUIDLen)
+    }
+
+    awaitingConns[uid] = conn
+    awaitingConnsMutex.Unlock()
+    conn.Write([]byte(uid))
+    conn.Close()
+}
+
+func handleUploader(conn net.Conn) {
+    uidBuffer := make([]byte, ProtoPeerUIDLen)
+    conn.Read(uidBuffer)
+    uid := string(uidBuffer)
+    awaitingConnsMutex.Lock()
+
+    for awaitingConns[uid] == nil {
+        awaitingConnsMutex.Unlock()
+        time.Sleep(time.Millisecond * 100)
+        awaitingConnsMutex.Lock()
+    }
+
+    awaitingConn := awaitingConns[uid]
+    awaitingConnsMutex.Unlock()
+    conn.Write([]byte(awaitingConn.RemoteAddr().String() + "\n"))
+    conn.Close()
+    delete(awaitingConns, uid)
+}
+
 func randSeq(n int) string {
-    const Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    letters := []rune("abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789")
 
     b := make([]rune, n)
 
     for i := range b {
-        b[i] = Letters[rand.Intn(len(Letters))]
+        b[i] = letters[rand.Intn(len(letters))]
     }
 
     return string(b)
