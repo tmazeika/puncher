4cb7d92d4cdcb188dd7c840392c607bb9b967424
T.J. Mazeika <t@mazeika.me>
2016-01-16 19:46:19 -0500

    Add #generateId

--- client/target.go
+++ client/target.go
@@ -1,7 +1,21 @@
 package client
 
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
 func (c *client) HandleTarget() error {
 
 
 	return nil
+}
+
+func generateId() (string, error) {
+	const Len = 16
+	idBuff := make([]byte, Len / 2)
+	if _, err := rand.Read(idBuff); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(idBuff), nil
 }
