201c431053f91ac932f5f7b919c71ceccc89daa9
T.J. Mazeika <t@mazeika.me>
2016-02-07 23:28:43 -0500

    Add ID validation to Client

--- src/main/java/me/mazeika/transhift/puncher/server/Client.java
+++ src/main/java/me/mazeika/transhift/puncher/server/Client.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 public class Client
 {
+    public static int ID_LEN = 16;
+
     /**
      * The attribute key, used when attaching the Client object to its context.
      */
@@ -14,6 +16,19 @@ public class Client
 
     private boolean dialing;
     private byte[] id;
+
+    /**
+     * Validates the given ID. The ID is valid if and only if its length is
+     * equal to {@link Client#ID_LEN}.
+     *
+     * @param id the ID to validate
+     *
+     * @return {@code true} if the ID is valid, {@code false} otherwise
+     */
+    public static boolean validateId(byte[] id)
+    {
+        return id.length == ID_LEN;
+    }
 
     /**
      * Gets if this client is trying to dial its peer.
