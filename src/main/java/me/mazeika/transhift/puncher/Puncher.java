2f9c930fbe6ccfc2925ac3ddedcb041be1d19fc3
T.J. Mazeika <t@mazeika.me>
2016-02-28 17:05:15 -0500

    Remove bad module

--- src/main/java/me/mazeika/transhift/puncher/Puncher.java
+++ src/main/java/me/mazeika/transhift/puncher/Puncher.java
@@ -5,7 +5,6 @@ import com.google.inject.Injector;
 import com.google.inject.Key;
 import me.mazeika.transhift.puncher.options.OptionsModule;
 import me.mazeika.transhift.puncher.pipeline.PipelineModule;
-import me.mazeika.transhift.puncher.pool.PoolModule;
 import me.mazeika.transhift.puncher.server.ServerModule;
 import me.mazeika.transhift.puncher.pipeline.Pipeline;
 import me.mazeika.transhift.puncher.server.Server;
@@ -25,8 +24,7 @@ public class Puncher
                 new ServerModule(),
                 new TagsModule(),
                 new HandlerModule(),
-                new MetaModule(),
-                new PoolModule());
+                new MetaModule());
 
         // add shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
